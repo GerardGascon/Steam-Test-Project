23d07d454119fbdfcb252dd16b4207f0596bdacd
Gerard Gascón <ggasconmoline@gmail.com>
2025-12-10 16:46:57 +0100

    Initial commit

---
+++ Assets/Tests/Tests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+using Steamworks;
+
+namespace Tests {
+	public class Tests {
+		[Test]
+		public void SteamInitWorks() {
+			Assert.That(SteamAPI.Init(), Is.True);
+		}
+	}
+}
